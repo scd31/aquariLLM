ab02fdc74fe8f00f695bbe198b8dd8c10cc01c6f
Stephen D <webmaster@scd31.com>
2025-01-25 12:31:33 -0500

    nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils?ref=main";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = (
+              with pkgs;
+              [
+                pkg-config
+                openssl
+              ]
+            );
+
+            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          };
+        }
+    );
+}
